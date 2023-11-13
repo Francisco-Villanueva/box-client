6eb1c58c297ea56908bb7e3389fecfa6ae97766a
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-11-13 13:47:16 -0300

    feat: create button common

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import { Button } from "commons/Button";
+
 export default function Home() {
   return (
-    <main >
+    <main>
+      <div className="m-10 flex flex-col gap-4 bg- p-4">
+        <Button>CREAR</Button>
+
+        <Button variant="secondary">INICIAR SESION</Button>
+      </div>
     </main>
-  )
+  );
 }
