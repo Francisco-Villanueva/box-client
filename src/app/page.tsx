e4d4320df4364205d6665181643f6905affc7452
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-11-13 13:47:41 -0300

    cleaning page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
 import { Button } from "commons/Button";
 
 export default function Home() {
-  return (
-    <main>
-      <div className="m-10 flex flex-col gap-4 bg- p-4">
-        <Button>CREAR</Button>
-
-        <Button variant="secondary">INICIAR SESION</Button>
-      </div>
-    </main>
-  );
+  return <main></main>;
 }
