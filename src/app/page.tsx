99ac9271c0648ab85468bc7fa718af2c999c2fdb
Mateo <mateo.navarro98@gmail.com>
2023-11-14 12:37:50 -0300

    fix: Correction in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
-import { TitleBox } from "commons";
-import { ArrowLeft, ShortArrowIcon } from "commons/Icons";
-
 export default function Home() {
-  return <main>
-    
-  </main>;
+  return <main></main>;
 }
