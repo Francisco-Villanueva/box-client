63481f2d63e85c0b54292e430211bda66233b0e9
Mateo <mateo.navarro98@gmail.com>
2023-11-15 10:53:47 -0300

    feat: Shipment Card Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,20 @@ import BoxLayout from "commons/BoxLayout";
 import { ShipmentCard } from "components/ShipmentCard";
 
 export default function Home() {
-  return <main>
-
-    
-    <div className="m-10">
-    <ShipmentCard idPackage={"#0A235"} addressPackage="Amenabar 2356, CABA"></ShipmentCard>
-    </div>
-  </main>;
+  return (
+    <main>
+      <div className="m-10">
+        <ShipmentCard
+          pack={{
+            _id: "#6c2614",
+            address: "9 de Julio 3100,",
+            clientName: "Miguel Rodr\u00edguez",
+            weight: 118.47,
+            deliverDate: "2023-21-11",
+            status: "EN CURSO",
+          }}
+        ></ShipmentCard>
+      </div>
+    </main>
+  );
 }
--- src/components/ShipmentCard.tsx
+++ src/components/ShipmentCard.tsx
@@ -1,26 +1,60 @@
-import { IconBox } from "commons";
+import { Button, IconBox } from "commons";
+import { TrashIcon } from "commons/Icons";
 import Status from "commons/Status";
 import { ReactNode } from "react";
 
-interface CardProps {
-  children?: ReactNode;
-  idPackage?: string;
-  addressPackage?: string;
+interface ShipmentCardProps {
+  pack: Pack;
 }
 
-export function ShipmentCard({
-  children,
-  idPackage,
-  addressPackage,
-}: CardProps) {
+interface User {
+  _id: string;
+  name: string;
+  lastName: string;
+  email: string;
+  password: string;
+  image: string;
+  role: string;
+  packages: Pack[];
+}
+interface Pack {
+  _id: string;
+  address: string;
+  clientName: string;
+  weight: number;
+  deliverDate: string;
+  status: string;
+}
+
+export function ShipmentCard({ pack }: ShipmentCardProps) {
   return (
-    <div className="font-roboto bg-white text-darkGreen w-80 border-black border-2 p-2 flex flex-row ">
-      {<IconBox />}
-      <div className="flex flex-col">
-        <>{idPackage}</>
-        <>{addressPackage}</>
+    <div className="font-roboto bg-white text-darkGreen w-80 border-black border-2 p-2 flex">
+      <div>{<IconBox />}</div>
+      <div className="font-roboto text-xs pl-2">
+        <div className="font-semibold">{pack._id}</div>
+        <div className="font-normal">{pack.address}</div>
+        <div>CABA</div>
       </div>
-      {<Status statusColor="green" statusShipment="Entregado"></Status>}
+      <div className="ml-auto">
+        <div>{<Status status={`${pack.status}`}></Status>}</div>
+
+        {pack.status === "EN CURSO" ? (
+          <div>
+            <Button
+              variant="secondary"
+              className=""
+            > <TrashIcon className="w-3"/> </Button>
+          </div>
+        ) : pack.status === "PENDIENTE" ? (
+          <div className="text-xs ">
+            <Button
+              variant="secondary"
+              children={"INICIAR"}
+              className=""
+            ></Button>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
