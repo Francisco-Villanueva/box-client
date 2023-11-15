56efb8048654fefa054c3f604cea6c898519d98a
Mateo <mateo.navarro98@gmail.com>
2023-11-15 11:21:14 -0300

    fix: page.tsx cleaned

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
-import BoxLayout from "commons/BoxLayout";
-import { ShipmentCard } from "components/ShipmentCard";
-
 export default function Home() {
   return (
-    <main>
-      <div className="m-10">
-        <ShipmentCard
-          pack={{
-            _id: "#6c2614",
-            address: "9 de Julio 3100,",
-            clientName: "Miguel Rodr\u00edguez",
-            weight: 118.47,
-            deliverDate: "2023-21-11",
-            status: "EN CURSO",
-          }}
-        ></ShipmentCard>
-      </div>
-    </main>
+    <main></main>
   );
 }
