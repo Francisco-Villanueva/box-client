8171086cae665a44cd1c87fb528f807c53d2716a
Mateo <mateo.navarro98@gmail.com>
2023-11-16 10:26:25 -0300

    feat: /Admin/packages view

--- src/app/admin/carriers/page.tsx
+++ src/app/admin/carriers/page.tsx
@@ -1,10 +1,33 @@
+"use client";
+import { ArrowLeft, BoxLayout, BoxTitle, Title, TitleBox } from "commons";
+import { DetailCard } from "components";
 import React from "react";
+import { user } from "../../../mocks/users.json";
 
 export default function page() {
   return (
     <div>
-      REPARTIDORES, LISTA DE TODOS LOS REPARTIDORES CON S SU PORCENTAGE, STATUS,
-      ETC
+      <div>
+        <TitleBox
+          variant="primary"
+          icon={<ArrowLeft />}
+          className="w-full my-2 pr-6"
+        >
+          reparto en curso
+        </TitleBox>
+        <BoxLayout
+          children={
+            <div>
+              <BoxTitle children="Enero"></BoxTitle>{" "}
+              {user.map((carrier)=>(
+                <DetailCard type="carrier" data={} />
+              ))}
+              
+              <BoxTitle variant="bottom"></BoxTitle>
+            </div>
+          }
+        ></BoxLayout>
+      </div>
     </div>
   );
 }
