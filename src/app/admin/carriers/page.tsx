62e5f40975f7a5ce8077c1de4510fc69cd8d5706
Mateo <mateo.navarro98@gmail.com>
2023-11-17 15:02:21 -0300

    fix: /admin/carriers view fixed with trimmer

--- src/app/admin/carriers/page.tsx
+++ src/app/admin/carriers/page.tsx
@@ -3,63 +3,78 @@ import {
   ArrowLeft,
   BoxLayout,
   BoxTitle,
+  Button,
   ShortArrowIcon,
   Title,
   TitleBox,
 } from "commons";
 import { CarrierCard } from "components";
-import React from "react";
+import React, { useState } from "react";
 import { observer } from "mobx-react-lite";
 import { useStore } from "models/root.store";
 import Link from "next/link";
 
 export default observer(function page() {
+  const [trimmer, setTrimmer] = useState(4);
   const {
     users: { carriers },
   } = useStore();
 
+  const handleTrimmer = () => {
+    if (trimmer === carriers.length) {
+      setTrimmer(4);
+    } else {
+      setTrimmer(carriers.length);
+    }
+  };
+
   return (
-    <div>
-      <div>
-        <TitleBox
-          variant="primary"
-          icon={
-            <Link href={"/admin"}>
-              <ArrowLeft />
-            </Link>
-          }
-          className="w-full my-2 pr-6"
+    <div className="h-[95%] flex flex-col gap-4 justify-between">
+      <TitleBox
+        variant="primary"
+        icon={
+          <Link href={"/admin"}>
+            <ArrowLeft />
+          </Link>
+        }
+        className="w-full"
+      >
+        repartidores
+      </TitleBox>
+
+      <BoxLayout className="h-[90%]">
+        <BoxTitle
+          variant="topDate"
+          className="justify-between h-[10%] p-6 items-center"
         >
-          repartidores
-        </TitleBox>
+          <Title>ENERO</Title>
+          <Title>Fecha</Title>
+        </BoxTitle>
 
-        <BoxLayout className="h-[75vh] overflow-scroll">
-          <BoxTitle
-            variant="topDate"
-            className="justify-between p-4 items-center"
-          >
-            <Title>Enero</Title>
-            <Title>mie/03</Title>
-          </BoxTitle>
-
-          {/* TODO ver el error de Type en PackageCheckboxCard */}
-          <div className="flex flex-col m-auto ">
-            {!carriers.length ? (
-              "loading"
-            ) : (
-              <>
-                {carriers.map((carrier) => (
-                  <CarrierCard carrier={carrier} />
-                ))}
-              </>
-            )}
-          </div>
-
-          <BoxTitle variant="bottom">
-            <ShortArrowIcon className="rotate-[270deg] w-6" />
-          </BoxTitle>
-        </BoxLayout>
-      </div>
+        {/* TODO ver el error de Type en PackageCheckboxCard */}
+        <div className="overflow-scroll max-h-[90%] flex flex-col m-auto">
+          {!carriers.length ? (
+            "loading"
+          ) : (
+            <>
+              {carriers.slice(0, trimmer).map((carrier) => (
+                <CarrierCard carrier={carrier} />
+              ))}
+            </>
+          )}
+        </div>
+        <BoxTitle variant="bottom" className="h-[10%]">
+          <Button className="border-none" variant="secondary"  onClick={handleTrimmer}>
+            <ShortArrowIcon
+              className={`transition-all duration-300 ${
+                trimmer === carriers.length
+                  ? " rotate-[90deg]"
+                  : " rotate-[270deg]"
+              } w-6`}
+            />
+          </Button>
+        </BoxTitle>
+      </BoxLayout>
     </div>
   );
 });
