2b74c932514fa6499cd77c6a228aebf091350841
Dario Beratti <darioberatti@gmail.com>
2023-12-21 17:08:35 -0300

    fix(carrier): Carrier status color red for 'Rejected' carriers

--- src/commons/CarrierStatus.tsx
+++ src/commons/CarrierStatus.tsx
@@ -3,12 +3,18 @@ interface StatusProps {
 	status?: string
 }
 export function CarrierStatus({ status }: StatusProps) {
+	let bgColor = ''
+	if (status === 'HABILITADO') {
+		bgColor = 'bg-lightGreen'
+	} else if (status === 'RECHAZADO') {
+		bgColor = 'bg-red-300'
+	} else {
+		bgColor = 'bg-lightGrey'
+	}
 	return (
 		<>
 			<div
-				className={`${
-					status === 'HABILITADO' ? 'bg-lightGreen' : 'bg-lightGrey'
-				} py-[.5] px-2 flex justify-center rounded-md gap-2 w-28`}>
+				className={`${bgColor} py-[.5] px-2 flex justify-center rounded-md gap-2 w-28`}>
 				<p className="text-center text-darkGreen font-roboto font-[500] text-xs uppercase ">
 					{status}
 				</p>
