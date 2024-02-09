3ae9cc9758b9abb12a474489d200623088ab1eb3
Mateo <mateo.navarro98@gmail.com>
2024-02-09 12:52:57 -0300

    fix: 404 page view

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { AppLayout, ArrowLeft, TitleBox } from 'commons'
+import { AppLayout, ArrowLeft, Title, TitleBox } from 'commons'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
 
 export default function Loading() {
 	const router = useRouter()
@@ -20,6 +21,22 @@ export default function Loading() {
 				className="w-full my-2 pr-6">
 				Página no encontrada
 			</TitleBox>
+
+			<div className="flex flex-col justify-between gap-2 h-full mt-20">
+				<section className="bg-white  flex items-center gap-2  p-4  rounded-2xl">
+					<Image
+						src={'/404.webp'}
+						alt="s"
+						width={100}
+						height={100}
+						className="rounded-2xl "
+					/>
+
+					<div>
+						<Title>Lo siento! La página que estas buscando no existe.</Title>
+					</div>
+				</section>
+			</div>
 		</AppLayout>
 	)
 }
